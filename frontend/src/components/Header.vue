15ae6fe3dd877fe1f23a43e9c34d5179dbe8c735
Akide <mail@llycloud.com>
2021-04-19 13:24:45 +0930

    Eslint

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,11 +3,11 @@
     app
     color="rgb(16, 37, 53)"
   >
-  <v-col
+    <v-col
       cols="12"
       sm="4"
     >
-    <v-hover
+      <v-hover
         v-slot="{ hover }"
         disabled
       >
@@ -18,41 +18,40 @@
           max-width="160"
           color="rgb(16, 37, 53)"
         >
-        <v-img
-        class="mx-2"
-        src="../assets/logo_uni.png"
-        height="400px"
-        contain
-        >
-        <div class="d-flex align-center">
-      </div>
-        </v-img>
+          <v-img
+            class="mx-2"
+            src="../assets/logo_uni.png"
+            height="400px"
+            contain
+          >
+            <div class="d-flex align-center" />
+          </v-img>
         </v-card>
       </v-hover>
-  </v-col>
-  <v-tabs
-    fixed-tabs
-    background-color="rgb(16, 37, 53)"
-    dark
-  >
-    <v-tab>
-      HOME
-    </v-tab>
-    <v-tab>
-      MAP
-    </v-tab>
-    <v-tab>
-      HOTSPOT
-    </v-tab>
-    <v-tab>
-      CONTACT
-    </v-tab>
-  </v-tabs>
-    <v-spacer></v-spacer>
+    </v-col>
+    <v-tabs
+      fixed-tabs
+      background-color="rgb(16, 37, 53)"
+      dark
+    >
+      <v-tab>
+        HOME
+      </v-tab>
+      <v-tab>
+        MAP
+      </v-tab>
+      <v-tab>
+        HOTSPOT
+      </v-tab>
+      <v-tab>
+        CONTACT
+      </v-tab>
+    </v-tabs>
+    <v-spacer />
     <div class="mx-5">
-    <v-btn color="white">
-      <span class="black--text">Log in</span>
-    </v-btn>
+      <v-btn color="white">
+        <span class="black--text">Log in</span>
+      </v-btn>
     </div>
     <v-btn color="#1976D2">
       Sign up
