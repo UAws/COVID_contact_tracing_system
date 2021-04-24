2a5de88d3baadc1468706f08672c1b73966f88aa
Akide <mail@llycloud.com>
2021-04-24 23:04:48 +0930

    HotFix : Header file merge failed in previous conflict resolving

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -80,17 +80,6 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos
 }
-
-var prevScrollpos = window.pageYOffset
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById('navbar').style.top = '0'
-  } else {
-    document.getElementById('navbar').style.top = '-180px'
-  }
-  prevScrollpos = currentScrollPos
-}
 </script>
 
 <style scoped>
