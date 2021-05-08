e94e376c7f799c9dec79a7b5cecc196547483392
Akide <mail@llycloud.com>
2021-05-09 03:48:36 +0930

    init : typeorm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,48 @@
-import './pre-start'; // Must be the first import
-import app from '@server';
-import logger from '@shared/Logger';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import {User} from "./entity/User";
 
+createConnection().then(async connection => {
 
-// Start the server
-const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
-});
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
+
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000);
+
+    // insert new users for test
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Timber",
+        lastName: "Saw",
+        age: 27
+    }));
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Phantom",
+        lastName: "Assassin",
+        age: 24
+    }));
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+
+}).catch(error => console.log(error));
