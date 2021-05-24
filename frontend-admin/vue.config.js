e787dec19e3241a7bc2bca82b7dd1096d77780a3
Akide <mail@llycloud.com>
2021-05-25 03:49:13 +0930

    configuration of build for production

--- frontend-admin/vue.config.js
+++ frontend-admin/vue.config.js
@@ -26,7 +26,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/admin/'
+    : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
