0a7ded3911de064f221b1044aa454844e443ef1f
Akide <mail@llycloud.com>
2021-05-26 23:52:18 +0930

    Modify backend and Add CI Support

--- backend/src/controller/UserInfoController.ts
+++ backend/src/controller/UserInfoController.ts
@@ -1,26 +1,46 @@
-import {getRepository} from "typeorm";
+import {getRepository, Like} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import {User} from "../entity/User";
+import {ApiResultBean} from "../support/ApiResultBean";
 
 export class UserInfoController {
 
     private userRepository = getRepository(User);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+
+
+        const limit = request.query.limit || 10
+        const page = request.query.page || 0
+        const keyword = request.query.keyword || ''
+        const sort = request.query.sort || 'ASC'
+
+        const [list, total] = await this.userRepository.findAndCount({
+            where: { username : Like('%' + keyword + '%') },
+            order: { user_id : sort},
+            take: limit,
+            skip: (page-1) * limit,
+            relations: ['Role']
+        });
+
+
+        return ApiResultBean.success({total, list});
+
+
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id, {relations: ['Role', 'UserCheckIn','Venue']});
+        return ApiResultBean.success(await this.userRepository.findOne(request.params.id, {relations: ['Role', 'UserCheckIn', 'Venue']}));
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+        return ApiResultBean.success(await this.userRepository.save(request.body));
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
         let userToRemove = await this.userRepository.findOne(request.params.id);
         await this.userRepository.remove(userToRemove);
+        return ApiResultBean.success();
     }
 
 }
---
+++ backend/src/support/ApiResultBean.ts
@@ -0,0 +1,49 @@
+import {log} from "util";
+
+export class ApiResultBean {
+
+
+    private static serialVersionUID : number = -8276264968757808344 ;
+
+    public static SUCCESS: number = 20000;
+
+    private message : string = "Request SUCCESS";
+
+    private code : number = ApiResultBean.SUCCESS;
+
+    private data : Object;
+
+
+    constructor(message: string, code: number, data: Object) {
+        this.message = message;
+        this.code = code;
+        this.data = data
+    }
+
+
+    public static success(data?: Object, message?: string, code?: number): ApiResultBean {
+
+
+        if (data != null && message != null && code != null) {
+            return new ApiResultBean(message, code, data);
+        }
+
+        if (data != null && message != null) {
+            return new ApiResultBean(message, this.SUCCESS, data);
+        }
+
+        if (data != null && code != null) {
+            return new ApiResultBean("Request SUCCESS", code, data);
+        }
+
+        if (data != null) {
+            return new ApiResultBean("Request SUCCESS", this.SUCCESS, data);
+        }
+
+        return new ApiResultBean("Request SUCCESS", this.SUCCESS, null);
+
+    }
+
+
+
+}
