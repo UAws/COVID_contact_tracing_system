1676c5839a9c2e1a44767ee6bcaa9e03acc55eb0
equis17 <q1158582010@qq.com>
2021-05-30 18:13:26 +0800

    fix

--- frontend-admin/src/api/venue.js
+++ frontend-admin/src/api/venue.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-export const listVenue = (params) => request.get('http://localhost:3000/api/venue/info', { params })
-export const editVenue = (id, data) => request.post(`http://localhost:3000/api/venue/venueID/${id}`, data)
-export const delVenue = (id) => request.delete(`http://localhost:3000/api/venue/info/${id}`)
-export const addVenue = (data) => request.post(`http://localhost:3000/api/venue/info`, data)
+export const listVenue = (params) => request.get('https://wdc-backend-live.dev.vmv.re/api/venue/info', { params })
+export const editVenue = (id, data) => request.post(`https://wdc-backend-live.dev.vmv.re/api/venue/venueID/${id}`, data)
+export const delVenue = (id) => request.delete(`https://wdc-backend-live.dev.vmv.re/api/venue/info/${id}`)
+export const addVenue = (data) => request.post(`https://wdc-backend-live.dev.vmv.re/api/venue/info`, data)
