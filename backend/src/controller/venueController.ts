580b1f4ceb23b1a4624e8c447883d6801096f1c5
equis17 <q1158582010@qq.com>
2021-05-30 18:07:56 +0800

    Revert "feat: venue table"
    This reverts commit 4dd0cbd8

--- backend/src/controller/venueController.ts
+++ backend/src/controller/venueController.ts
@@ -1,46 +1,18 @@
-import { NextFunction, Request, Response } from 'express'
-import StatusCodes from 'http-status-codes'
-import { getRepository, Like } from 'typeorm'
-import { Venue } from '../entity/Venue'
-import { ApiResultBean } from '../support/ApiResultBean'
-
-const { BAD_REQUEST, CREATED, OK } = StatusCodes
+import {getRepository} from "typeorm";
+import {NextFunction, Request, Response} from "express";
+import StatusCodes from 'http-status-codes';
+import {Venue} from "../entity/Venue";
+const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
 export class venueController {
-  private venueRepository = getRepository(Venue)
+    private venueRepository = getRepository(Venue);
 
-  async all(request: Request, response: Response, next: NextFunction) {
-    const limit = Number(request.query.limit) || 10
-    const page = Number(request.query.page) || 1
-    const keyword = request.query.keyword || ''
-    const sort = request.query.sort || 'ASC'
+    async getVenueInfo(request: Request, response: Response, next: NextFunction){
+        return this.venueRepository.findOne(request.params.id, {relations: ['Users']});
+    }
 
-    const [list, total] = await this.venueRepository.findAndCount({
-      where: { venue_name: Like('%' + keyword + '%') },
-      order: { venue_id: sort },
-      take: limit,
-      skip: (page - 1) * limit,
-      relations: ['Users'],
-    } as any)
-    return ApiResultBean.success({ total, list })
-  }
-
-  async getVenueInfo(request: Request, response: Response, next: NextFunction) {
-    return this.venueRepository.findOne(request.params.id, { relations: ['Users'] })
-  }
-
-  async postVenueInfo(request: Request, response: Response, next: NextFunction) {
-    return this.venueRepository.save(request.body)
-  }
-
-  async save(request: Request, response: Response, next: NextFunction) {
-    return ApiResultBean.success(await this.venueRepository.save(request.body))
-  }
-
-  async remove(request: Request, response: Response, next: NextFunction) {
-    let venueToRemove = await this.venueRepository.findOne(request.params.id)
-    await this.venueRepository.remove(venueToRemove)
-    return ApiResultBean.success()
-  }
+    async postVenueInfo(request: Request, response: Response, next: NextFunction) {
+        return this.venueRepository.save(request.body);
+    }
 
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ createConnection().then(async connection => {
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Credentials', true);
 
         // Pass to next layer of middleware
         next();
