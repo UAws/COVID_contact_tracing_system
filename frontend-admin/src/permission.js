fdb217395f91a4fd514994e35d7ba865750abe41
Akide <mail@llycloud.com>
2021-06-02 17:27:41 +0930

    Add : implement the interaction with login

--- frontend-admin/src/permission.js
+++ frontend-admin/src/permission.js
@@ -34,7 +34,9 @@ router.beforeEach(async(to, from, next) => {
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const { roles } = await store.dispatch('user/getInfo')
+          const { Role } = await store.dispatch('user/getInfo')
+
+          const roles = [Role[0].role_name]
 
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
@@ -48,7 +50,9 @@ router.beforeEach(async(to, from, next) => {
         } catch (error) {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
+          Message.error('Has Error')
+
+          console.log(error)
           next(`/login?redirect=${to.path}`)
           NProgress.done()
         }
--- frontend-admin/src/utils/request.js
+++ frontend-admin/src/utils/request.js
@@ -19,8 +19,11 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      // config.headers['X-Token'] = getToken()
+      const token = JSON.parse(getToken())
+      config.headers['Authorization'] = token.tokenType + ' ' + token.accessToken
     }
+    config.headers['Content-Type'] = 'application/json'
     return config
   },
   error => {
