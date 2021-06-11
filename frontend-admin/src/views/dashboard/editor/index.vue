deae19f773f29981bc65865206b1aa7796c1f9aa
Ziqi222 <64782437+Ziqi222@users.noreply.github.com>
2021-06-11 15:53:38 +0930

    Modified user dashboard & swap the image (#64)
    * Dashboard Edited with relative info
    
    * Add home link to our logo, modify some layouts
    
    * Add socia media in Footer
    
    * Fixed Dashboard layout
    
    * Fix: remove unremoved components
    
    * Delete pic
    
    * Dashboard and Image modifies
    
    Co-authored-by: Akide <mail@llycloud.com>

--- frontend-admin/src/views/dashboard/editor/index.vue
+++ frontend-admin/src/views/dashboard/editor/index.vue
@@ -1,20 +1,90 @@
 <template>
-  <div class="dashboard-editor-container">
-    <div class=" clearfix">
-      <pan-thumb :image="avatar" style="float: left">
-        Your roles:
-        <span v-for="item in roles" :key="item" class="pan-info-roles">{{ item }}</span>
-      </pan-thumb>
-      <github-corner style="position: absolute; top: 0px; border: 0; right: 0;" />
-      <div class="info-container">
-        <span class="display_name">{{ name }}</span>
-        <span style="font-size:20px;padding-top:20px;display:inline-block;">Editor's Dashboard</span>
+  <el-row :gutter="20">
+    <div class="dashboard-editor-container">
+      <div class=" clearfix">
+        <pan-thumb :image="avatar" style="float: left">
+          Your roles:
+          <span v-for="item in roles" :key="item" class="pan-info-roles">{{ item }}</span>
+        </pan-thumb>
+        <github-corner style="position: absolute; top: 0px; border: 0; right: 0;" />
+        <div class="info-container">
+          <span style="font-size:40px;" class="display_name">{{ name }}</span>
+          <span style="font-size:25px;padding-top:20px;display:inline-block;">Welcome!</span>
+        </div>
       </div>
+      <v-row :gutter="10">
+        <img :src="emptyGif" class="emptyGif">
+
+        <v-card
+          class="mx-auto"
+          max-width="400"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://lh3.googleusercontent.com/proxy/YilJI16h1beNfsmjjmRLRKk5lOZbS7NR5FJG2TGFN4HuBrSiub3kdgOez5T-cEof9dCsJDrNBKrTv557ultTK9RcYX-xQeH6cE7ivYxBaz-PCmbP7GP0kAs"
+          />
+
+          <v-card-title>
+            South Australia
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>15&deg;C</div>
+
+            <v-icon>mdi-cloud</v-icon>
+          </v-card-text>
+          <v-list-item
+            link
+            @click="goToProfile()"
+          >
+            <v-list-item-icon>
+              <v-icon
+                size="30"
+              >
+                mdi-account-circle
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title class="text-body-2"> View Profile </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            link
+            @click="goToCheckIn()"
+          >
+            <v-list-item-icon>
+              <v-icon
+                size="30"
+              >
+                mdi-clock-start
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title> Check In </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            link
+            @click="goToHome()"
+          >
+            <v-list-item-icon>
+              <v-icon
+                size="30"
+              >
+                mdi-home
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title> Home </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-row>
     </div>
-    <div>
-      <img :src="emptyGif" class="emptyGif">
-    </div>
-  </div>
+  </el-row>
 </template>
 
 <script>
@@ -27,7 +97,13 @@ export default {
   components: { PanThumb, GithubCorner },
   data() {
     return {
+      // emptyGif: 'https://media0.giphy.com/media/QuslVm3lEHtubdaJ1u/giphy.gif'
       emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
+      // items: [
+      //   { icon: 'mdi-account-circle', title: 'Profile' },
+      //   { icon: 'mdi-clock-start', title: 'Check-in' },
+      //   { icon: 'mdi-email', title: 'Inbox' }
+      // ]
     }
   },
   computed: {
@@ -36,6 +112,17 @@ export default {
       'avatar',
       'roles'
     ])
+  },
+  methods: {
+    goToProfile() {
+      this.$router.push('/profile/index')
+    },
+    goToCheckIn() {
+      this.$router.push('/user/check-in')
+    },
+    goToHome() {
+      this.$router.push('/documentation/index')
+    }
   }
 }
 </script>
@@ -43,10 +130,12 @@ export default {
 <style lang="scss" scoped>
   .emptyGif {
     display: block;
-    width: 45%;
-    margin: 0 auto;
+    width: 50%;
+    padding: 20px 20px 0px;
   }
-
+  .mx-auto {
+    background-color: #fcfcfcd7;
+  }
   .dashboard-editor-container {
     background-color: #e3e3e3;
     min-height: 100vh;
